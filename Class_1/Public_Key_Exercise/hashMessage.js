5875f5faf6fcf2ba6f566749580a73462862d1f8
Cripto_Hash <104468656+CriptoHash@users.noreply.github.com>
2025-03-20 02:48:16 -0300

    Update hashMessage.js

--- Class_1/Public_Key_Exercise/hashMessage.js
+++ Class_1/Public_Key_Exercise/hashMessage.js
@@ -5,13 +5,15 @@ const { utf8ToBytes, toHex } = require("ethereum-cryptography/utils");
 
 function hashMessage(helloWorldHex) {
 
-    // turn this into an array of bytes, the expected format for the hash algorithm
+    // transforme isso em uma matriz de bytes, o formato esperado para o algoritmo de hash
     const bytes = utf8ToBytes(helloWorldHex);
-    // hash the message using keccak256
+    // hash da mensagem usando keccak256
     const hash = keccak256(bytes);
 
-    console.log(toHex(hash)); // 928c3f25193b338b89d5646bebbfa2436c5daa1d189f9c565079dcae379a43be
+    console.log(toHex(hash)); // 47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad
     return hash;
 }
 
 module.exports = hashMessage;
+
+// should return the keccak256 hash of hello world - Test Passed
