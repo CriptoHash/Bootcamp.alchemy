86672e9f9b6152e6f1bf703e379b94985f5bcca6
Cripto_Hash <104468656+CriptoHash@users.noreply.github.com>
2025-04-04 03:40:47 -0300

    Create 5_hashMessage.js

---
+++ Class_1/Public_Key_Exercise/2_RecoverKey/5_hashMessage.js
@@ -0,0 +1,16 @@
+// Criado no exercicio HashMessage
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { utf8ToBytes, toHex } = require("ethereum-cryptography/utils");
+
+function hashMessage(helloWorldHex) {
+
+    // turn this into an array of bytes, the expected format for the hash algorithm
+    const bytes = utf8ToBytes(helloWorldHex);
+    // hash the message using keccak256
+    const hash = keccak256(bytes);
+
+    console.log(toHex(hash)); // 928c3f25193b338b89d5646bebbfa2436c5daa1d189f9c565079dcae379a43be
+    return hash;
+}
+
+module.exports = hashMessage;
